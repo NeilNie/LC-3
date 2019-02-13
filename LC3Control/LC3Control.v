a73200d992621486cd65e35507e7db1ca9ff1788
Neil Nie <yongyang.nie@gmail.com>
2019-02-13 09:07:15 -0500

    implemented instruction fetch, add, and, not

--- LC3Control/LC3Control.v
+++ LC3Control/LC3Control.v
@@ -25,14 +25,14 @@ module LC3Control(
 input [15:0] IR;
 input clk, N,Z,P;
 	
-output reset, enaALU;
-output regWE, enaMARM, selMAR, selEAB1, enaPC;
-output ldPC, ldIR, ldMAR, ldMDR, selMDR, memWE, enaMDR;
-output [1:0] aluControl, selPC, selEAB2;
-output [2:0] SR1, SR2, DR;
+output reg reset, enaALU;
+output reg regWE, enaMARM, selMAR, selEAB1, enaPC;
+output reg ldPC, ldIR, ldMAR, ldMDR, selMDR, memWE, enaMDR;
+output reg [1:0] aluControl, selPC, selEAB2;
+output reg [2:0] SR1, SR2, DR;
 
 reg [5:0] current_state;
-reg [5:0] next_state;
+reg [5:0] J;
 
 
 always @ (posedge clk) begin
@@ -43,43 +43,100 @@ always @ (posedge clk) begin
 	
 	// state #: 18
 	if (current_state == 6'b010010) begin
+		
 		// MAR <- PC
 		// PC <- PC + 1
-		next_state <= 6'b100001;
+		enaPC <= 1;
+		ldMAR <= 1;
+		selPC <= 2'b00;
+		ldPC <= 1;
+		
+		J <= 6'b100001;
 	end
 	
 	// state #: 33
 	else if (current_state == 6'b100001) begin
+	
 		// MDR <- M
-		next_state <= 6'b100011;
+		selMDR <= 1;
+		ldMDR <= 1;
+		enaPC <= 0; 	// close PC tri state buffer
+		
+		J <= 6'b100011;
 	end
 	
 	// state #: 35
 	else if (current_state == 6'b100011) begin
 		
 		// IR <- MDR
-		next_state <= 6'b100000;
+		enaMDR <= 1;
+		ldIR <= 1;
+		
+		J <= 6'b100000;
 	end
-	
 	// end ============== instruction fetch ===================
+	
 	// 
 	//
+	
 	// begin ============= instruction parse ==================
 	
 	else if (current_state == 6'b100000) begin
-	
+		J <= {2'b00, IR[15:12]};
 	end
 	// end =============== instruction parse ==================
 	//
+	//
 	// begin ============ instruction execute =================
-
 	
+	// state #: 1 (ADD)
+	else if (current_state == 6'b000001) begin
+		
+		// DR <- SR1 + Op
+		DR <= IR[11:9];
+		SR1 <= IR[8:6];
+		SR2 <= IR[2:0];
+		aluControl <= 2'b01;
+		
+		enaALU <= 1;
+		regWE <= 1;
+		
+		J <= 6'b010010;
+	end
+	
+	// state #: 5 (AND)
+	else if (current_state == 6'b000101) begin
+		
+		// DR <- SR1 & Op
+		DR <= IR[11:9];
+		SR1 <= IR[8:6];
+		SR2 <= IR[2:0];
+		aluControl <= 2'b10;
+		J <= 6'b010010;
+		
+		enaALU <= 1;
+		regWE <= 1;
+	end
+	
+	// state #: 9 (NOT)
+	else if (current_state == 6'b001001) begin
+		
+		// DR <- NOT SR1
+		DR <= IR[11:9];
+		SR1 <= IR[8:6];
+		aluControl <= 2'b11;
+		J <= 6'b010010;
+		
+		enaALU <= 1;
+		regWE <= 1;
+		
+	end
 
 end
 
 always @ (negedge clk) begin
 	
-	current_state <= next_state;
+	current_state <= J;
 end
 
 endmodule
